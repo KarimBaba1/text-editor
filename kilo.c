36d52656a6fe6caaf974b1480358dc04a5704526
Karim Baba <240167470@stu.vtc.edu.hk>
2025-11-16 00:57:50 +0800

    scroll-limits

--- kilo.c
+++ kilo.c
@@ -292,6 +292,8 @@ char buf[32];
 /*** input ***/
 
 void editorMoveCursor(int key){
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
     switch (key) {
         case ARROW_LEFT:
             if(E.cx != 0){
@@ -299,7 +301,9 @@ void editorMoveCursor(int key){
             }
             break;
         case ARROW_RIGHT:
+            if (row && E.cx < row->size) {
                 E.cx++;
+            }
             break;
         case ARROW_UP:
             if(E.cy != 0){
